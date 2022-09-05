e3e913f587283df800c8656715149ff026d711bd
AlexAseyev <alexeyas13@gmail.com>
2022-09-05 23:11:19 +0300

    add: room body

---
+++ TrapRoomGame/Assets/Scripts/TrapRespawnDownRoom.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrapRespawnDownRoom : MonoBehaviour
+{
+    //public GameObject m_RespawnPoint;
+
+    void OnTriggerEnter()
+    {
+        print("respawn");
+    }
+}
